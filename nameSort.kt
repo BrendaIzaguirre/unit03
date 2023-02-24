d322055acd33af761eee7a6e75e2e1f2569a1754
BrendaIzaguirre <112352653+BrendaIzaguirre@users.noreply.github.com>
2023-02-24 09:05:15 -0600

    Create nameSort.kt

---
+++ nameSort.kt
@@ -0,0 +1,9 @@
+fun main() {
+    val people = listOf(people("Charlie",54), people("Leslie", 34),
+            people("Andy", 11))
+     
+    val sortedPeople = people.sortedBy { people -> people.name }
+    sortedPeople.forEach { p -> println(p.name) }
+}
+
+class people(val name: String,marks : Int)
