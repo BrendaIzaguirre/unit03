c7d23ef346798d68f3978d2618277f53ba6e94d0
BrendaIzaguirre <112352653+BrendaIzaguirre@users.noreply.github.com>
2023-02-24 08:56:36 -0600

    Create magicNumbers.kt

---
+++ magicNumbers.kt
@@ -0,0 +1,13 @@
+fun main() {
+  
+var month = int('Enter a month (in numeric form): ');
+var day = int('Enter a day: '); 
+var year = int('Enter two-digit year: ');
+
+if (month * day == year) {
+    print('This date is magic!')
+    }
+else {
+    print('This date is not magic!')
+	}
+}
